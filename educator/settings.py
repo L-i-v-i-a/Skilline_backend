3ba28988342cfa24483844bcf49e1742222f8d90
olivia <oliviaoguelina@gmail.com>
2026-03-01 09:00:09 +0100

    The accounts app is done

--- educator/settings.py
+++ educator/settings.py
@@ -238,14 +238,4 @@ if not os.getenv('DATABASE_URL'):
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')          # your@gmail.com
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # the 16-char app password
-
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'your@gmail.com')
-SERVER_EMAIL = DEFAULT_FROM_EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
